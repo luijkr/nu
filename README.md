f4b91953dd95eac582cb4dd6235ad19b6c70d0cd
luijkr <luijk.r@gmail.com>
2018-01-26 14:53:46 +0100

    restructuring, added nltk

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# nu
+# NU.nl scraper
+
+### Objective of this repository
+
+Text.
+
+### Prerequisites
+
+Optional (but recommended), create a virtual environment.
+
+```
+virtualenv --python $(which python3) venv
+source venv/bin/activate
+```
+
+Install Python packages.
+
+```
+pip install -r requirements.txt
+```
+
+Pull docker images for Splash and MongoDB.
+
+```
+docker pull scrapinghub/splash
+docker pull mongo
+```
+
+Run both images in the background.
+
+```
+docker run -d -p 8050:8050 -p 5023:5023 scrapinghub/splash
+docker run -d -p 27017:27017 --name mongodb mongo
+```
---
+++ scraper/main.py
@@ -0,0 +1,133 @@
+"""Scraping news articles from NU.nl."""
+
+import requests
+import itertools
+import pymongo
+from bs4 import BeautifulSoup
+
+
+def get_page_links(block):
+    """Finding links for content block."""
+    # list all items in block, unless advertorial
+    items = [
+        li for li in block.findAll('li')
+        if 'advertorial' not in li.attrs['data-sac-marker']
+    ]
+
+    # extract urls from item
+    item_urls = [
+        'http://www.nu.nl' + item.find('a').attrs['href']
+        for item in items if 'hasvideo' not in item.attrs['class']
+    ]
+
+    return item_urls
+
+
+def extract_article(page_link):
+    """Scrape article text."""
+    # make request using splash
+    print(page_link)
+    r = requests.post(
+        'http://localhost:8050/render.html',
+        json={
+            'url': page_link,
+            'http_method': 'POST',
+            'body': 'a=b',
+            'wait': 0.5
+        }
+    )
+
+    # parse html
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    # article title
+    article_title = soup.find('div', class_='title')
+    try:
+        article_title = article_title.find('h1').text.strip(' \n ')
+    except:
+        article_title = article_title.text.strip(' \n ')
+
+    # find block with article text
+    article_body = soup.find('div', class_='block article body')
+
+    # extract all paragraph text
+    article_text = ' '.join([
+        p.text.encode('ascii', 'ignore').decode('ascii')
+        for p in article_body.findAll('p')
+    ])
+
+    # put in dict
+    article = {
+        'article_title': article_title,
+        'article_text': article_text
+    }
+
+    return article
+
+
+def get_articles(category):
+    """Get links to listed articles."""
+    # create overview link
+    url_overview = 'http://www.nu.nl/{}'.format(category)
+
+    # make request using splash
+    r = requests.post(
+        'http://localhost:8050/render.html',
+        json={
+            'url': url_overview,
+            'http_method': 'POST',
+            'body': 'a=b',
+            'wait': 0.5
+        }
+    )
+
+    # parse html
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    # find page links
+    blocks = soup.findAll('div', class_='block articlelist')
+    page_links = list(itertools.chain(*[
+        get_page_links(block) for block in blocks
+    ]))
+
+    print('{} articles found in "{}", scraping.'.format(
+        str(len(page_links)), category))
+
+    # extract article
+    articles = [extract_article(page_link) for page_link in page_links]
+
+    # add category
+    for i in range(len(articles)):
+        articles[i]['article_category'] = category
+
+    return articles
+
+
+def main(categories):
+    """Run main program."""
+    print('Start scraper for NU.nl.')
+
+    # get current articles
+    articles = [get_articles(category) for category in categories]
+    articles = list(itertools.chain(*articles))
+
+    print('Scraping done, {} articles succesfully scraped'.format(
+        str(len(articles))))
+    print('Storing in database.')
+
+    # connect to MongoDB
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+
+    # grab database
+    db = client['newsarticles']
+
+    # add collection
+    collection = db['NU']
+
+    # add articles to database
+    collection.insert_many(articles)
+
+
+# run main program
+categories = ['economie', 'sport', 'tech', 'entertainment', 'lifestyle']
+main(categories)
