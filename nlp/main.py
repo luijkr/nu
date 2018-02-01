0d35113f47f2b5cc976f1b835388b87f729ddd82
rluijk <r.luijk@lumc.nl>
2018-02-01 17:30:27 +0100

    read files, not db

--- nlp/main.py
+++ nlp/main.py
@@ -2,7 +2,7 @@
 
 import re
 import json
-import pymongo
+import glob
 
 from functools import partial
 from collections import Counter
@@ -11,8 +11,6 @@ from nltk import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction import FeatureHasher
 
 
@@ -61,18 +59,6 @@ def process_article(article_text):
     return stems
 
 
-def get_tfidf(words):
-    """Calculate TF-IDF matrix."""
-    # get word counts
-    count_vect = CountVectorizer(strip_accents='unicode')
-    counts = count_vect.fit_transform([' '.join(w) for w in words])
-
-    # Term-Frequency Inverse-Document Frequency transformation
-    tfidf = TfidfTransformer(sublinear_tf=True).fit_transform(counts)
-
-    return tfidf
-
-
 def scale_dict(d):
     """Scale a single dictionary to [0, 1]."""
     # extract values and keys
@@ -107,10 +93,32 @@ def feature_hashing(words, n_features):
     h = FeatureHasher(n_features=n_features)
 
     # transform into numeric array
-    f = h.transform(norm_counts)
+    f = h.transform(norm_counts).toarray()[0]
 
     return f
 
+
+def add_hash(fname):
+    """Perform feature hashing and add to file."""
+    # read file
+    article = json.load(open(fname))
+
+    # process article text
+    words = process_article(
+        article['article_text'] + article['article_title'])
+
+    # feature hashing
+    hashed = list(feature_hashing(words=[words], n_features=n_features))
+
+    # add data to file
+    article['hashed_vec'] = hashed
+
+    # write file
+    with open(fname, 'w') as file:
+        json.dump(article, file)
+
+    file.close()
+    
 
 # define NL tokenizer
 tokenizer = partial(word_tokenize, language='dutch')
@@ -127,26 +135,13 @@ if include_custom:
 else:
     stop_words = set(stop_words_nltk)
 
-# connect to MongoDB
-client = pymongo.MongoClient('mongodb://localhost:27017/')
+# list of article paths
+article_paths = glob.glob('articles/*.json')
 
-# grab collection from within database
-conn = client['newsarticles']['NU']
-
-# extract words and categories
-words = []
-identifier = []
-for article in conn.find({'article_text': {'$ne': ''}}):
-    words.append(process_article(
-        article['article_text'] + article['article_title']))
-    identifier.append(article['_id'])
-
-# get TF-IDF
-tfidf = get_tfidf(words)
-
-# feature hashing
+# extract words, apply hash function, store in same object
 n_features = 2**10
-hashed = feature_hashing(words=words, n_features=10)
-
-hashed[10,:].toarray()
-feature_hashing(words=[words[10]], n_features=10).toarray()
+for path in article_paths:
+    try:
+        add_hash(path)
+    except:
+        pass
